870ddace0fb220165aefdaef7c57ea768c10b25e
fernando tenorio <fnd.tenorio@gmail.com>
2014-12-09 12:50:26 -0200

    update Test

--- Test.java
+++ Test.java
@@ -45,7 +45,7 @@ public class Test{
 		System.out.println("Done spliting moves");
 		//Parse
 		PrintWriter writer = new PrintWriter("mobility_control.csv", "UTF-8");
-		writer.print("white_central,white_mobility,black_central,black_mobility");
+		writer.print("wc_open,wc_mid,wc_end,wm_open,wm_mid,wm_end,bc_open,bc_mid,bc_end,bm_open,bm_mid,bm_end");
 		int i = 0;
 		for (String pgn : games){
 			writer.println();
@@ -53,9 +53,43 @@ public class Test{
 			PGNStats stats = new PGNStats(moves);
 			int[][] data = stats.centralDominance();
 
-			writer.print(mean(data[0]) + "," + mean(data[1]) + "," + mean(data[2]) + "," + mean(data[3]));
+			float wc_open_avg = 0.0f;
+			float wm_open_avg = 0.0f;
+			float bc_open_avg = 0.0f;
+			float bm_open_avg = 0.0f;
+
+			float wc_mid_avg = 0.0f;
+			float wm_mid_avg = 0.0f;
+			float bc_mid_avg = 0.0f;
+			float bm_mid_avg = 0.0f;
+
+			float wc_end_avg = 0.0f;
+			float wm_end_avg = 0.0f;
+			float bc_end_avg = 0.0f;
+			float bm_end_avg = 0.0f;
+
+			wc_open_avg = mean(Arrays.copyOfRange(data[0], 0, OPEN_LIMIT));
+			wm_open_avg = mean(Arrays.copyOfRange(data[1], 0, OPEN_LIMIT));
+			bc_open_avg = mean(Arrays.copyOfRange(data[2], 0, OPEN_LIMIT));
+			bm_open_avg = mean(Arrays.copyOfRange(data[3], 0, OPEN_LIMIT));
+
+			if (data[0].length > OPEN_LIMIT){
+				wc_mid_avg = mean(Arrays.copyOfRange(data[0], OPEN_LIMIT, MID_LIMIT));
+				wm_mid_avg = mean(Arrays.copyOfRange(data[1], OPEN_LIMIT, MID_LIMIT));
+				bc_mid_avg = mean(Arrays.copyOfRange(data[2], OPEN_LIMIT, MID_LIMIT));
+				bm_mid_avg = mean(Arrays.copyOfRange(data[3], OPEN_LIMIT, MID_LIMIT));
+			}
+			if (data[0].length > MID_LIMIT){
+				wc_end_avg = mean(Arrays.copyOfRange(data[0], MID_LIMIT, data[0].length));
+				wm_end_avg = mean(Arrays.copyOfRange(data[1], MID_LIMIT, data[0].length));
+				bc_end_avg = mean(Arrays.copyOfRange(data[2], MID_LIMIT, data[0].length));
+				bm_end_avg = mean(Arrays.copyOfRange(data[3], MID_LIMIT, data[0].length));
+			}
+			
+			writer.print(wc_open_avg + "," + wc_mid_avg + "," + wc_end_avg + "," + wm_open_avg + "," + wm_mid_avg + "," + wm_end_avg + "," +
+				bc_open_avg + "," + bc_mid_avg + "," + bc_end_avg + "," + bm_open_avg + "," + bm_mid_avg + "," + bm_end_avg);
 			i++;
-			if (i % 1000 == 0)
+			if (i % 5000 == 0)
 				System.out.format("Done %.1f%s%n", i*100.0f/games.length, "%");
 		}
 		writer.close();
